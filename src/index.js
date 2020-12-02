cdb21e2a0cd0bb7023d304190c14441aed45910c
Shubhojeet Banerjee <shubhojeet@gmail.com>
2020-12-02 17:56:14 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService"
+import QuestionBox from "./components/Questionbox";
+import Result from "./components/Result"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class QuizBee extends Component{
+    state = {
+        questionBank : [],
+        score: 0,
+        responses: 0
+    };
+    getquestionsMethod = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank : question
+            })
+        })
+    }
+    computeAnswer = (answers, correctAnswer) => {
+        if(answers === correctAnswer){
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ?this.state.responses + 1: 5
+        });
+    }
+    playAgain = () => {
+        this.getquestionsMethod();
+        this.setState({
+            score : 0,
+            responses :0
+        });
+    };
+    componentDidMount(){
+        this.getquestionsMethod();
+    }
+    render(){
+        return (
+            <div className ="container">
+                <div className="title">QuizBee</div>
+                {this.state.questionBank.length > 0 &&
+                this.state.responses < 5 &&
+                this.state.questionBank.map(({question, answers, correct, questionId}) => 
+                (<QuestionBox 
+                    question={question} 
+                    option={answers} 
+                    key={questionId}
+                    selected={answers => this.computeAnswer(answers, correct)} />
+                ))}
+                {this.state.responses === 5 ? (<Result score = {this.state.score} playAgain = {this.playAgain} />) : null}
+            </div>
+        )
+    };
+}
+ReactDOM.render(<QuizBee /> , document.getElementById("root"));
